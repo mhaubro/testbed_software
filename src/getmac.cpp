39652ae13b24c1a747d32d8407f5993d50f0901a
Martin Haubro <martin.haubro@hotmail.com>
2019-05-08 16:31:53 +0200

    Change to mac-address finding to work on rpis

--- src/getmac.cpp
+++ src/getmac.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <iostream>
 #include <cstring>
+#include "execcmd.h"
 
 bool ismac(std::string str, std::size_t found){
 	//We didn't get a match
@@ -10,12 +11,12 @@ bool ismac(std::string str, std::size_t found){
 	}
 
 	//A mac is more than 17 characters from the first ':'.
-	if (str.length() < (found + 17)){
+	if (str.length() < (found + 23)){
 		return false;
 	}
 
-	for (int i = 1; i < 5; i++){
-		if (str[found+3*i] != ':' ){
+	for (int i = 0; i < 5; i++){
+		if (str[found+8+3*i] != ':' ){
 				std::cout << "No mac found\n";
 			return false;
 		}
@@ -26,17 +27,16 @@ bool ismac(std::string str, std::size_t found){
 //Takes an 18-byte or larger char array, prints the mac in the first 17 separated with _, instead of :, to be used for folders.
 bool getMacAddress(char * uc_Mac)
 {
-	std::ifstream ifs("/proc/net/arp");
-	std::string content( (std::istreambuf_iterator<char>(ifs) ),
-                       (std::istreambuf_iterator<char>()    ) );
 
-	std::size_t found = content.find(":");//This should be part of the first Mac address to be found
+	std::string content = exec("ifconfig");
+
+	std::size_t found = content.find("ether ");//This should be part of the first Mac address to be found
 
 	if (!ismac(content, found)){
 		return false;
 	}
 
-	memcpy((void *) uc_Mac, (void *) &content[found-2], 17);
+	memcpy((void *) uc_Mac, (void *) &content[found+6], 17);
 	for (int i = 1; i < 6; i++){
 		uc_Mac[3*i-1] = '_';
 	}
