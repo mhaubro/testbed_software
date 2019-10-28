7038c7d6e2fc9509bfd84bb40c27d77e8cce38e0
Martin Haubro <martin.haubro@hotmail.com>
2019-10-28 13:50:10 +0100

    updating installation sequence

--- install.sh
+++ install.sh
@@ -2,3 +2,8 @@
 sudo apt-get install grabserial automake pkg-config libtool libusb-1.0-0-dev gcc-8 g++-8 curl
 git clone https://github.com/mhaubro/ti_openocd_fixed.git openocd-ti && cd openocd-ti/openocd && sudo autoreconf -f -i && ./configure && make && sudo make install && sudo mv /usr/local/bin/openocd /usr/bin/openocd
 curl https://rclone.org/install.sh | sudo bash
+sudo cp testbed-software.service testbed-update-service.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable testbed-software.service
+sudo systemctl enable testbed-update-service.service
+sudo reboot
